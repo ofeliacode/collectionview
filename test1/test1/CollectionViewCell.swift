60ce9b7622115b5c1928f86ec2e4fb241e16ac9d
ofelicode <dir3ktt@yandex.ru>
2020-09-22 17:11:24 +0300

    2

--- test1/test1/CollectionViewCell.swift
+++ test1/test1/CollectionViewCell.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class CustomViewCell: UICollectionViewCell {
     static let identifier = "customCellIdentifier"
+    //линия
+    let line: UIView = {
+        let line = UIView(frame: CGRect(x: 0, y: 48, width:  (self.frame.width) - 17 , height: 1 / UIScreen.main.scale))
+        line.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.4)
+        return line
+    }()
+    //имя
+    let labelName: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.contentView.frame.width, height: 21))
+        label.textAlignment = .left
+        label.text = "name:"
+        return label
+    }()
+    //цена
+    let labelPrice: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 25, width: self.contentView.frame.width, height: 21))
+        label.textAlignment = .left
+        label.textColor = UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
+        label.text = "price:"
+        return label
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
-        
+        addSubview(labelName)
+        addSubview(labelPrice)
+        addSubview(line)
     }
     
     required init?(coder: NSCoder) {
--- test1/test1/R.generated.swift
+++ test1/test1/R.generated.swift
@@ -114,37 +114,6 @@ struct R: Rswift.Validatable {
   }
   #endif
 
-  /// This `R.info` struct is generated, and contains static references to 1 properties.
-  struct info {
-    struct uiApplicationSceneManifest {
-      static let _key = "UIApplicationSceneManifest"
-      static let uiApplicationSupportsMultipleScenes = false
-
-      struct uiSceneConfigurations {
-        static let _key = "UISceneConfigurations"
-
-        struct uiWindowSceneSessionRoleApplication {
-          struct defaultConfiguration {
-            static let _key = "Default Configuration"
-            static let uiSceneConfigurationName = infoPlistString(path: ["UIApplicationSceneManifest", "UISceneConfigurations", "UIWindowSceneSessionRoleApplication", "Default Configuration"], key: "UISceneConfigurationName") ?? "Default Configuration"
-            static let uiSceneDelegateClassName = infoPlistString(path: ["UIApplicationSceneManifest", "UISceneConfigurations", "UIWindowSceneSessionRoleApplication", "Default Configuration"], key: "UISceneDelegateClassName") ?? "$(PRODUCT_MODULE_NAME).SceneDelegate"
-            static let uiSceneStoryboardFile = infoPlistString(path: ["UIApplicationSceneManifest", "UISceneConfigurations", "UIWindowSceneSessionRoleApplication", "Default Configuration"], key: "UISceneStoryboardFile") ?? "Main"
-
-            fileprivate init() {}
-          }
-
-          fileprivate init() {}
-        }
-
-        fileprivate init() {}
-      }
-
-      fileprivate init() {}
-    }
-
-    fileprivate init() {}
-  }
-
   /// This `R.string` struct is generated, and contains static references to 2 localization tables.
   struct string {
     /// This `R.string.launchScreen` struct is generated, and contains static references to 0 localization keys.
